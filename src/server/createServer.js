49731b767f3974f286221cb060b213c44ae840ff
David Chavez <depch47@gmail.com>
2024-08-16 09:35:20 -0500

    Add cors any origin

--- src/server/createServer.js
+++ src/server/createServer.js
@@ -3,13 +3,17 @@
 import express from "express";
 import { routes } from "../routes/index.js";
 import deserializeUser from "../middleware/deserializeUser.js";
-
+import cors from "cors"
 
 export function createServer() {
   const app = express()
 
   app.use(express.json())
   app.use(deserializeUser)
+
+  app.use(cors({
+    origin: "*"
+  }))
 
   routes(app)
 
